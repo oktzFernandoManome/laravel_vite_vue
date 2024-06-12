2c44b86c4d1613b86f911b2e964479a33e4aa3f3
fernando.gomes@oktz.com.br <fernando.manome@gmail.com>
2024-06-12 10:12:53 -0300

    ajustes App component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from "vue"
+
+const name = ref("")
+</script>
+
 <template>
-    <h2>App from vue</h2>
+    <p>{{name}}</p>
+    <input type="text" v-model="name">
 </template>
